bec3e839715db4ff9f5a953da6ec3347bd4e3818
Dan Cadden <Shikkic@gmail.com>
2019-07-17 04:16:25 -0400

    fix up test script

--- nand2tetris/projects/01/test.sh
+++ nand2tetris/projects/01/test.sh
@@ -2,20 +2,29 @@
 
 set -e
 
-# Elementary Chips
-echo "Testing Elementary Chips..."
-elem_chips=(Not And Or Xor Mux Dmux)
-for chip in ${elem_chips[@]}; do
-    echo "Running ${chip}.tst"
-    ../../tools/HardwareSimulator.sh "${chip}.tst"
-done
-echo ""
+test_chip_sets () {
+    local chipset_name=$1
+    local chip_names=$2[@]
+    chip_names=("${!chip_names}")
 
-# 16-bit variants
-echo "Testing 16-bit variant chips..."
-sixteen_bit_chips=(Not16 And16 Or16 Mux16)
-for chip in ${sixteen_bit_chips[@]}; do
-    echo "Running ${chip}.tst"
-    ../../tools/HardwareSimulator.sh "${chip}.tst"
-done
-echo ""
+    echo "Testing ${chipset_name}..."
+    for chip in ${chip_names[@]}; do
+        echo "Running ${chip}.tst"
+        ../../tools/HardwareSimulator.sh "${chip}.tst"
+    done
+
+    echo ""
+}
+
+main () {
+    elem_chips=(Not And Or Xor Mux Dmux)
+    test_chip_sets "Elementary Chips" elem_chips
+
+    sixteen_bit_chips=(Not16 And16 Or16 Mux16)
+    test_chip_sets "16-bit variant chips" sixteen_bit_chips
+
+    multiway_variant_chips=(Or8Way)
+    test_chip_sets "Or8Way" multiway_variant_chips 
+}
+
+main "$@"
