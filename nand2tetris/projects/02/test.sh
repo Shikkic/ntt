6118a7cadf25f9eb87fa2dfdb037ae8984d395bd
Dan Cadden <Shikkic@gmail.com>
2019-09-05 21:39:07 -0400

    create basic test suite for project 2

---
+++ nand2tetris/projects/02/test.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+set -e
+
+test_chip_sets () {
+    local chipset_name=$1
+    local chip_names=$2[@]
+    chip_names=("${!chip_names}")
+
+    echo "Testing ${chipset_name}..."
+    for chip in ${chip_names[@]}; do
+        echo "Running ${chip}.tst"
+        ../../tools/HardwareSimulator.sh "${chip}.tst"
+    done
+
+    echo ""
+}
+
+main () {
+    addr_chips=(HalfAdder)
+    test_chip_sets "Adder Chips" addr_chips
+}
+
+main "$@"
